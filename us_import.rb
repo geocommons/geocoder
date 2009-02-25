c735808f414441471d02a03f7515b2a4521c43ea
Schuyler Erle <sderle@goldman.(none)>
2009-02-24 20:25:13 -0500

    Further progress on parsing TIGER files and munging into SQLite.

--- us_import.rb
+++ us_import.rb
@@ -1,132 +1,324 @@
+require 'rubygems'
 require 'sqlite3'
 require 'geo_ruby'
+require 'text'
 
 module Geocoder
 end
 
 module Geocoder::US
-  class Table
-    def initialize (db)
-      @db = db
-    end
-
-    def create
-      table = self.class.to_s.sub(/.*:/,"").downcase
-      columns = fields.map {|k,v| k + " " + v}.join ",\n    "
-      sql = "CREATE TABLE #{table} (\n    #{columns});\n"
-      for idx in indexes
-        idxname = table + "_"+ idx.gsub(/\W/,"_") + "_idx"
-        sql += "CREATE INDEX #{idxname} ON #{table} (#{idx});\n"
-      end
-      puts sql
-      @db.execute_batch(sql)
-    end
-
-    def fields
-      []
-    end
-
-    def indexes
-      []
-    end
-  end
-
-  class Places < Table
-    def fields
-      {
-        "plcidfp"     => "INTEGER(7) PRIMARY KEY",
-        "name"        => "VARCHAR(100)",
-        "name_phone"  => "VARCHAR(100)",
-        "name_full"   => "VARCHAR(100)"
-      }
-    end
-
-    def indexes
-      ["name"]
-    end
-  end
-
-  class Edges < Table
-    def fields
-      {
-        "tlid" => "INTEGER(10) PRIMARY KEY",
-        "lfromadd" => "INTEGER(6)",     # From House # (left side)
-        "ltoadd" => "INTEGER(6)",     # To House #   (left side)
-        "rfromadd" => "INTEGER(6)",     # From House # (right side)
-        "rtoadd" => "INTEGER(6)",     # To House #   (right side)
-        "geometry" => "BLOB"
-      }
-    end
-  end
-
-  class Features < Table
-    def fields
-      {
-        "tlid" => "INTEGER(10)",    # TIGER/Line ID
-        "name" => "VARCHAR(100)",   # Base portion of name
-        "name_phone" => "VARCHAR(100)",   # Metaphone hash of name
-        "predir" => "VARCHAR(2)",     # Prefix direction component
-        "pretyp" => "VARCHAR(3)",     # Prefix type component
-        "prequal" => "VARCHAR(2)",     # Prefix qualifier component
-        "sufdir" => "VARCHAR(2)",     # Suffix direction component
-        "suftyp" => "VARCHAR(3)",     # Suffix type component
-        "sufqual" => "VARCHAR(2)",     # Suffix qualifier component
-        "paflag" => "BOOLEAN",        # Primary/Alternate flag
-        "place" => "INTEGER(7)",     # FIPS place ID
-        "zip" => "INTEGER(5)"      # ZIP code
-      }
-    end
-    def indexes
-      ["name"]
-    end
-  end
-
-  class Ranges < Table
-    def fields
-      {
-        "tlid" => "INTEGER(10)",    # TIGER/Line ID
-        "fromhn" => "INTEGER(6)",     # From House #
-        "tohn" => "INTEGER(6)",     # To House #
-        "prefix" => "VARCHAR(12)",    # House number prefix
-        "zip" => "INTEGER(5)",     # ZIP code
-        "plus4" => "INTEGER(5)",     # ZIP plus 4
-        "side" => "CHAR(1)",        # Side flag 
-        "fromtyp" => "CHAR(1)",        # From address range end type
-        "totyp" => "CHAR(1)"         # To address range end type
-      }
-    end
-    def indexes
-      ["tlid"]
-    end
-  end
-
-  class Tiger
+  class Database
     def initialize (filename)
-      @shp = GeoRuby::Shp4r::ShpFile(filename)
+      @db = SQLite3::Database.new( filename )
+      @tables = table_classes.map {|cls| cls.new()}
+    end
+
+    def table_classes
+      table_module = Tables
+      table_class  = table_module.const_get("Table")
+      table_module.constants.map {|cname| table_module.const_get(cname)} \
+                       .find_all {|cls| cls.superclass == table_class}
+    end
+
+    def prepare_inserts
+      @inserts = @tables.map {|t| @db.prepare(t.insert)}
+    end
+
+    def import (shape, cache)
+      prepare_inserts if @inserts.nil?
+      @tables.zip(@inserts).each { |table, insert|
+        if table.copy? shape.data
+          table.munge(shape, cache).each {|row| insert.execute(row)} 
+        end
+      }
+    end
+
+    def import_all (source)
+      @db.transaction { source.each {|shape| import(shape, source.cache) } }
+    end
+
+    def create_all
+      @tables.each {|t| @db.execute_batch(t.create)}
+    end
+
+    def close
+      @db.close
     end
   end
 
-  class AllLines < Tiger
-    def fields
-      []
-    end
-  end
+  module Tables
+    class Table
+      def initialize
+        @table  = self.class.name.sub(/.*:/,"").downcase
+        @fields = fields
+      end
 
-  def self.open_db(filename)
-    SQLite3::Database.new( filename )
-  end
+      def name
+        @table
+      end
 
-  def self.create_all(filename)
-    db = self.open_db(filename)
-    for cls_name in self.constants
-      cls = self.const_get(cls_name)
-      if cls.superclass == Geocoder::US::Table
-        table = cls.new(db) 
-        table.create unless table.fields.empty?
+      def create
+        columns = @fields.map {|k,v| k + " " + v}.join ",\n    "
+        sql = "CREATE TABLE #{@table} (\n    #{columns});\n"
+        for idx in indexes
+          idxname = @table + "_"+ idx.gsub(/\W/,"_") + "_idx"
+          sql += "CREATE INDEX #{idxname} ON #{@table} (#{idx});\n"
+        end
+        return sql
+      end
+
+      def insert
+        columns = @fields.keys
+        places = columns.map {|col| ":"+col}.join(",")
+        "INSERT INTO #{@table} (#{columns.join ","}) VALUES (#{places});"
+      end
+
+      def fields
+        []
+      end
+
+      def indexes
+        []
+      end
+
+      def munge(shape, cache)
+        data = @fields.map {|k,v| shape.data[k.upcase]}
+        [Hash[*@fields.keys.zip(data).flatten]]
+      end
+
+      def copy?(shape)
+        false
       end
     end
-    db.close
+
+    class Places < Table
+      def fields
+        {
+          "plcidfp"     => "INTEGER(7)",
+          "name"        => "VARCHAR(100)",
+          "name_phone"  => "VARCHAR(100)"
+        }
+      end
+
+      def indexes
+        ["plcidfp", "name", "name_phone"]
+      end
+
+      def copy?(data)
+        data.key? "PLCIDFP"
+      end
+
+      def munge (record, cache)
+        results = super(record, cache)
+        # sometimes Census Statistical Areas have hyphenated names...
+        # if so, we want to split on the hyphen and create multiple records
+        # with each piece of the corresponding name.
+        for result in results
+          result["name_phone"] = Text::Metaphone.metaphone(result["name"])
+        end
+        results
+      end
+    end
+
+    class Edges < Table
+      def fields
+        {
+          "tlid" => "INTEGER(10) PRIMARY KEY",
+          "lfromadd" => "INTEGER(6)",     # From House # (left side)
+          "ltoadd" => "INTEGER(6)",     # To House #   (left side)
+          "rfromadd" => "INTEGER(6)",     # From House # (right side)
+          "rtoadd" => "INTEGER(6)",     # To House #   (right side)
+          "geometry" => "BLOB"
+        }
+      end
+
+      def copy?(data)
+        data.key? "TFIDR"
+      end
+
+      def munge(record, cache)
+        results = super(record, cache)
+        for result in results
+          result["geometry"] = SQLite3::Blob.new(shape.geometry.as_wkb)
+        end
+        results
+      end
+    end
+
+    class Features < Table
+      def fields
+        {
+          "tlid" => "INTEGER(10)",    # TIGER/Line ID
+          "name" => "VARCHAR(100)",   # Base portion of name
+          "name_phone" => "VARCHAR(100)",   # Metaphone hash of name
+          "predir" => "VARCHAR(2)",     # Prefix direction component
+          "pretyp" => "VARCHAR(3)",     # Prefix type component
+          "prequal" => "VARCHAR(2)",     # Prefix qualifier component
+          "sufdir" => "VARCHAR(2)",     # Suffix direction component
+          "suftyp" => "VARCHAR(3)",     # Suffix type component
+          "sufqual" => "VARCHAR(2)",     # Suffix qualifier component
+          "paflag" => "BOOLEAN",        # Primary/Alternate flag
+          "place" => "INTEGER(7)",     # FIPS place ID
+          "zip" => "INTEGER(5)"      # ZIP code
+        }
+      end
+
+      def indexes
+        ["name"]
+      end
+
+      def copy?(data)
+        data.key? "LINEARID"
+      end
+
+      def munge(record, cache)
+        records = super(record, cache)
+        results  = []
+        for record in records
+          record["name_phone"] = Text::Metaphone.metaphone(record["name"])
+          tlid = records["tlid"]
+          locales = cache.line2place[tlid].map {|place|
+                      cache.line2zip[tlid].map {|zip| [place,zip]}}
+          locales.uniq!
+          for place, zip in locales
+            result = record.dclone
+            result["place"] = place
+            result["zip"] = zip
+            results << result
+          end
+        end
+        results
+      end
+    end
+
+    class Ranges < Table
+      def fields
+        {
+          "tlid" => "INTEGER(10)",    # TIGER/Line ID
+          "fromhn" => "INTEGER(6)",     # From House #
+          "tohn" => "INTEGER(6)",     # To House #
+          "prefix" => "VARCHAR(12)",    # House number prefix
+          "zip" => "INTEGER(5)",     # ZIP code
+          "plus4" => "INTEGER(5)",     # ZIP plus 4
+          "side" => "CHAR(1)",        # Side flag 
+          "fromtyp" => "CHAR(1)",        # From address range end type
+          "totyp" => "CHAR(1)"         # To address range end type
+        }
+      end
+      def indexes
+        ["tlid"]
+      end
+      def copy?(data)
+        data.key? "FROMHN"
+      end
+    end
   end
 
+  module Tiger
+    class Cache
+      attr :face2place
+      attr :line2face
+      attr :line2zip
   
+      def reset!
+        @face2place = {}
+        @line2place = {}
+        @line2zip   = {}
+      end
+      
+      alias initialize reset!
+    end
+
+    class Shp
+      attr :cache
+      def initialize (filename, cache=nil)
+        cache = Cache.new() if cache.nil?
+        @shp = GeoRuby::Shp4r::ShpFile.open(filename + "_" + suffix)
+        @cache = cache
+      end
+      def each
+        @shp.each {|record| 
+          add_to_cache record.data
+          yield record
+        }
+      end
+      def add_to_cache (record)
+      end
+    end
+
+    class Dbf < Shp
+      def initialize (filename, cache=nil)
+        cache = Cache.new() if cache.nil?
+        filename += "_" + suffix + ".dbf"
+        @dbf = GeoRuby::Shp4r::Dbf::Reader.open(filename)
+        @cache = cache
+      end
+      def record (idx)
+        @dbf.record(idx)
+      end
+      def each
+        for i in 0 ... @dbf.record_count
+          record = @dbf.record(i)
+          add_to_cache record
+          yield GeoRuby::Shp4r::ShpRecord.new(nil,record)
+        end
+      end
+    end
+
+    class CurrentPlaces < Dbf
+      def suffix
+        "place"
+      end    
+    end
+
+    class AllLines < Shp
+      def suffix
+        "edges"
+      end
+      def add_to_cache(record)
+        @cache.line2place[record["TLID"]] = [
+          @cache.face2place[record["TFIDL"]], 
+          @cache.face2place[record["TFIDR"]]
+        ]
+        @cache.line2zip[record["TLID"]] = [
+          record["ZIPL"],
+          record["ZIPR"]  
+        ] 
+      end
+    end
+
+    class AddressRanges < Dbf
+      def suffix
+        "addr"
+      end
+    end
+
+    class FeatureNames < Dbf
+      def suffix
+        "featnames"
+      end
+    end 
+
+    class TopoFaces < Dbf
+      def suffix
+        "faces"
+      end
+      def add_to_cache(record)
+        @cache.face2place[record["TFID"]] = record["STATEFP"]+record["PLACEFP"]
+      end
+    end
+
+    class County
+      def initialize (filename)
+        @filename = filename
+        @cache = Cache.new()
+      end
+      def import
+        for cls in [TopoFaces, AllLines, FeatureNames, AddressRanges]
+          source = cls.new(@filename, @cache)
+          db.import_all(source)
+        end
+      end
+    end
+  end
 end
