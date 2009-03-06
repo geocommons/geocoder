5d029ce59abb01a1ad6a8756c0d9f9e1225906fc
Schuyler Erle <sderle@goldman.(none)>
2009-03-06 13:57:30 -0500

    Refactoring to put the burden of data processing on the tiger module rather than the database module.

---
+++ us/database.rb
@@ -0,0 +1,149 @@
+require 'rubygems'
+require 'sqlite3'
+require 'geo_ruby'
+require 'text'
+
+module Geocoder
+end
+
+module Geocoder::US
+  class Database
+    def initialize (filename)
+      @db = SQLite3::Database.new( filename )
+      @tables = table_classes
+    end
+
+    def table_classes
+      table_module = Geocoder::US
+      table_class  = table_module.const_get("Table")
+      table_module.constants.map {|cname| table_module.const_get(cname)} \
+                       .find_all {|cls| cls.superclass == table_class}
+    end
+
+    def prepare_inserts
+      Hash[@tables.map {|t| [t,@db.prepare(t.insert)]}];
+    end
+
+    def insert (record)
+      @insert = prepare_inserts if @insert.nil?
+      @insert[record.class].execute(record.values)
+    end
+
+    def transact
+      @db.transaction { yield }
+    end
+
+    def transaction
+      @db.transaction
+    end
+
+    def commit
+      @db.commit
+    end
+
+    def create_all
+      @tables.each {|t| @db.execute_batch(t.create)}
+    end
+
+    def close
+      @db.close
+    end
+  end
+
+  class Table
+    class << self
+      def field (name, type="VARCHAR(255)",idx=false)
+        @fields ||= []
+        @fields << name
+        @types ||= []
+        @types << type
+        index name if idx
+        send :attr, name, true
+      end
+      def fields
+        @fields
+      end
+      def index (name)
+        @indexes ||= []
+        @indexes << name
+      end
+      def table
+        @table ||= name.sub(/.*:/,"").downcase
+      end
+      def create
+        columns = fields.zip(@types).map {|k,v| k.to_s+" "+v}.join ",\n  "
+        sql = "CREATE TABLE #{table} (\n  #{columns});\n"
+        for idx in @indexes
+          idxname = table + "_"+ idx.to_s + "_idx"
+          sql += "CREATE INDEX #{idxname} ON #{table} (#{idx});\n"
+        end
+        return sql
+      end
+      def insert
+        columns = fields.map {|f| f.to_s}.join(",")
+        places  = (["?"] * fields.length).join(",")
+        "INSERT INTO #{table} (#{columns}) VALUES (#{places});"
+      end
+    end
+    def initialize (data={})
+      for field in self.class.fields
+        self.send((field.to_s+"=").to_sym, data[field])
+      end
+    end
+    def values
+      self.class.fields.map {|f| self.send f}
+    end
+    def set_name (value)
+      @name_phone = Text::Metaphone.metaphone(value)
+      @name = value
+    end
+  end
+
+  class Place < Table
+    field :plcidfp, "INTEGER(7)", true
+    field :name, "VARCHAR(100)", true
+    field :name_phone, "VARCHAR(100)", true
+    alias name= set_name 
+  end
+
+  class Edge < Table
+    # ordinarily tlid is unique, but edges can be duplicated across counties
+    # where a road forms part of a county border. it'll slow the import process
+    # to have to check each one before inserting, and it'll be even slower
+    # to have to do the inserts outside a transaction where the duplicate pkey
+    # exception can be caught...
+    #
+    # field :tlid, "INTEGER(10) PRIMARY KEY", true # TIGER/Line ID
+    field :tlid, "INTEGER(10)", true # TIGER/Line ID
+    field :geometry, "BLOB"
+
+    def geometry=(value)
+      @geometry = SQLite3::Blob.new(value.as_wkb)
+    end
+  end
+
+  class Feature < Table
+    field :tlid, "INTEGER(10)", true    # TIGER/Line ID
+    field :name, "VARCHAR(100)", true   # Base portion of name
+    field :name_phone, "VARCHAR(100)", true  # Metaphone hash of name
+    field :predir, "VARCHAR(2)"       # Prefix direction component
+    field :pretyp, "VARCHAR(3)"       # Prefix type component
+    field :prequal, "VARCHAR(2)"      # Prefix qualifier component
+    field :sufdir, "VARCHAR(2)"       # Suffix direction component
+    field :suftyp, "VARCHAR(3)"       # Suffix type component
+    field :sufqual, "VARCHAR(2)"      # Suffix qualifier component
+    field :paflag, "BOOLEAN"          # Primary/Alternate flag
+    field :place, "INTEGER(7)"        # FIPS place ID
+    field :zip, "INTEGER(5)", true    # ZIP code
+    alias name= set_name 
+  end
+
+  class Range < Table
+    field :tlid, "INTEGER(10)", true    # TIGER/Line ID
+    field :fromhn, "INTEGER(6)"         # From House #
+    field :tohn, "INTEGER(6)"           # To House #
+    field :prefix, "VARCHAR(12)"        # House number prefix
+    field :zip, "INTEGER(5)", true      # ZIP code
+    field :side, "CHAR(1)"              # Side flag 
+  end
+end
--- us/tiger.rb
+++ us/tiger.rb
@@ -3,15 +3,9 @@ require 'geo_ruby'
 require 'zip/zip'
 require 'tmpdir'
 require 'find'
-
-module Geocoder
-end
+require 'us/database'
 
 module Geocoder::US
-end
-
-
-module Geocoder::US::Tiger
   class Cache
     attr :face2place
     attr :line2place
@@ -30,26 +24,57 @@ module Geocoder::US::Tiger
 
   class Shp
     attr :cache
+    class << self
+      def suffix (value)
+        @suffix = value
+      end
+      def file (prefix, extension)
+        prefix + "_" + @suffix.to_s + "." + extension
+      end
+      def map (fields)
+        @field_map ||= {}
+        @field_map.merge! fields
+      end
+      def field (name)
+        @field_map ||= {}
+        name = name.downcase.to_sym
+        @field_map[name] || name
+      end
+      def target (cls)
+        @target = cls
+      end
+      def target_class
+        @target
+      end
+    end
     def initialize (filename, cache=nil)
       cache = Cache.new() if cache.nil?
-      @shp = GeoRuby::Shp4r::ShpFile.open(filename + "_" + self.class.suffix)
+      @shp = GeoRuby::Shp4r::ShpFile.open self.class.file(filename, "shp")
       @cache = cache
     end
     def each
-      @shp.each {|record| 
-        add_to_cache record.data
-        yield record
-      }
+      @shp.each {|record| yield record}
     end
-    def add_to_cache (record)
+    def process_each 
+      each {|record| yield process convert(record) if use? record.data}
+    end
+    def use? (data)
+      true
+    end
+    def convert (record)
+      data = {:geometry => record.geometry}
+      record.data.each {|k,v| data[self.class.field(k)] ||= v}
+      data
+    end
+    def process (data)
+      self.class.target_class.new(data)
     end
   end
 
   class Dbf < Shp
     def initialize (filename, cache=nil)
       cache = Cache.new() if cache.nil?
-      filename += "_" + self.class.suffix + ".dbf"
-      @dbf = GeoRuby::Shp4r::Dbf::Reader.open(filename)
+      @dbf = GeoRuby::Shp4r::Dbf::Reader.open self.class.file(filename, "dbf")
       @cache = cache
     end
     def record (idx)
@@ -57,56 +82,75 @@ module Geocoder::US::Tiger
     end
     def each
       for i in 0 ... @dbf.record_count
-        record = @dbf.record(i)
-        add_to_cache record
-        yield GeoRuby::Shp4r::ShpRecord.new(nil,record)
+        yield GeoRuby::Shp4r::ShpRecord.new(nil,record(i))
       end
     end
   end
 
   class CurrentPlaces < Dbf
-    def self.suffix
-      "place"
-    end    
+    suffix :place
+    target Place
   end
 
   class AllLines < Shp
-    def self.suffix
-      "edges"
+    suffix :edges
+    target Edge
+    def use? (data)
+      data["MTFCC"].start_with? "S" and data["FULLNAME"].any?
     end
-    def add_to_cache(record)
-      @cache.line2place[record["TLID"]] = [
-        @cache.face2place[record["TFIDL"]], 
-        @cache.face2place[record["TFIDR"]]
+    def process_place (data)
+      @cache.line2place[data[:tlid]] = [
+        @cache.face2place[data[:tfidl]], 
+        @cache.face2place[data[:tfidr]]
       ]
-      @cache.line2zip[record["TLID"]] = [
-        record["ZIPL"],
-        record["ZIPR"]  
+      @cache.line2zip[data[:tlid]] = [
+        data[:zipl],
+        data[:zipr]  
       ] 
+      Edge.new(data)
     end
   end
 
   class AddressRanges < Dbf
-    def self.suffix
-      "addr"
-    end
-    def add_to_cache(record)
-      @cache.tlids[record["TLID"]] = true
-    end
+    suffix :addr
+    target Range
   end
 
   class FeatureNames < Dbf
-    def self.suffix
-      "featnames"
+    suffix :featnames
+    target Feature
+    map :predirabrv => :predir
+    map :pretypabrv => :pretyp
+    map :prequalabr => :prequal
+    map :sufdirabrv => :sufdir
+    map :suftypabrv => :suftyp
+    map :sufqualabr => :sufqual
+    def use? (data)
+      data["MTFCC"].start_with? "S" and data["NAME"].any?
+    end
+    def process_places (record)
+      tlid = record[:tlid]
+      locales = []
+      for place in cache.line2place[tlid]
+        for zip in cache.line2zip[tlid]
+          locales << [place,zip]
+        end
+      end
+      locales.uniq!
+      for place, zip in locales
+        result = record.dclone
+        result[:place] = place
+        result[:zip] = zip
+        results << Feature.new(result)
+      end
+      results
     end
   end 
 
   class TopoFaces < Dbf
-    def self.suffix
-      "faces"
-    end
-    def add_to_cache(record)
-      @cache.face2place[record["TFID"]] = record["STATEFP"]+record["PLACEFP"]
+    suffix :faces
+    def process (data)
+      []
     end
   end
 
@@ -115,8 +159,9 @@ module Geocoder::US::Tiger
       @path = path
     end
     def import_file (cls, db)
-      archive, = Dir["#{@path}/*_#{cls.suffix}.zip"]
-      throw "can't find #{cls.suffix} ZIP file in #{@path}" if archive.nil?
+      glob = cls.file("#{@path}/*", "zip")
+      archive, = Dir[glob]
+      throw "can't find ZIP file #{glob}" if archive.nil?
       Dir.mktmpdir {|dir|
         Zip::ZipFile::open(archive) { |zf|
            zf.each {|file| zf.extract(file, File.join(dir, file.name)) }
@@ -124,12 +169,15 @@ module Geocoder::US::Tiger
         archive[/_[a-z]+\.zip$/] = ""
         extracted = File.join(dir, File.basename(archive))
         source = cls.new(extracted, @cache)
-        db.import_all(source)
+        source.process_each {|records|
+          records = [records] unless records.is_a? Array
+          records.each {|record| db.insert(record)}
+        }
       }
     end
     def import (db)
       puts "importing places from " + @path
-      import_file(CurrentPlaces, db)
+      # db.transact { import_file(CurrentPlaces, db) }
       Find.find(@path) {|dir|
         County.new(dir).import(db) if dir != @path and File.directory? dir
       }
@@ -142,15 +190,16 @@ module Geocoder::US::Tiger
       @cache = Cache.new()
     end
     def import (db)
-        puts "importing " + @path
-        for cls in [TopoFaces, AddressRanges, AllLines, FeatureNames]
-          puts "  loading " + cls.suffix
+      puts "importing " + @path
+      #db.transact {
+        #for cls in [TopoFaces, AddressRanges, AllLines, FeatureNames]
+      db.transaction
+        for cls in [AddressRanges, AllLines, FeatureNames]
+          puts "  loading " + cls.name
           import_file(cls, db)
         end
+      db.commit
+      #}
     end
-  end
-
-  def import (path)
-
   end
 end
