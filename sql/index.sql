3ce7b3750d560e82b63d28c836c53d28775f5f60
Schuyler Erle <schuyler@nocat.net>
2009-03-10 13:27:10 -0400

    Echo SQL commands on index build.

--- sql/index.sql
+++ sql/index.sql
@@ -1,5 +1,6 @@
-create edge_tlid_idx on edge (tlid);
-create feature_tlid_idx on feature (tlid);
-create feature_name_zip_idx on feature (name, zip);
-create feature_name_phone_zip_idx on feature (name_phone, zip);
-create range_tlid_idx on range (tlid);
+.echo on
+create index edge_tlid_idx on edge (tlid);
+create index feature_tlid_idx on feature (tlid);
+create index feature_name_zip_idx on feature (name, zip);
+create index feature_name_phone_zip_idx on feature (name_phone, zip);
+create index range_tlid_idx on range (tlid);
