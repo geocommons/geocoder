828ef3eb229f64611848de7739ac481089d76a7c
Schuyler Erle <sderle@goldman.(none)>
2009-03-15 14:03:13 -0400

    Make numeric prefix and suffixes actually work.

--- src/libsqlite3_geocoder/util.c
+++ src/libsqlite3_geocoder/util.c
@@ -14,23 +14,24 @@ int address_metaphone(const char *input, char *output, int max_phones) {
     }
 }
 
-size_t rindex_nondigit (const char *string) {
-    size_t i = strlen(string);
-    if (!i) return 0;
-    for (i--; i && isdigit(string[i]); i--);
-    if (!isdigit(string[i])) i++;
+signed int rindex_nondigit (const char *string) {
+    signed int i = strlen(string);
+    if (!i) return -1;
+    for (i--; i >= 0 && isdigit(string[i]); i--);
     return i;
 }
 
-size_t digit_suffix (const char *input, char *output) {
-    size_t i = rindex_nondigit(input);
-    strcpy(output, input+i);
+signed int digit_suffix (const char *input, char *output) {
+    signed int i = rindex_nondigit(input);
+    strcpy(output, input+i+1);
     return strlen(output);
 }
 
-size_t nondigit_prefix (const char *input, char *output) {
-    size_t i = rindex_nondigit(input);
-    if (i) strncpy(output, input, i-1);
-    output[i] = '\0';
+signed int nondigit_prefix (const char *input, char *output) {
+    signed int i = rindex_nondigit(input);
+    if (i++ >= 0) {
+        strncpy(output, input, i);
+        output[i] = '\0';
+    }
     return i;
 }
