3ad47017d8a212fb553edc4d07cd14c944b4bbc7
Schuyler Erle <schuyler@nocat.net>
2009-03-15 14:24:38 -0400

    Add tuning params to index.sql (?) and remove the name index on feature.

--- sql/index.sql
+++ sql/index.sql
@@ -1,6 +1,10 @@
 .echo on
-create index edge_tlid_idx on edge (tlid);
-create index feature_tlid_idx on feature (tlid);
-create index feature_name_zip_idx on feature (name, zip);
-create index feature_name_phone_zip_idx on feature (name_phone, zip);
-create index range_tlid_idx on range (tlid);
+PRAGMA temp_store=MEMORY;
+PRAGMA journal_mode=MEMORY;
+PRAGMA synchronous=OFF;
+PRAGMA cache_size=500000;
+PRAGMA count_changes=0;
+CREATE INDEX edge_tlid_idx ON edge (tlid);
+CREATE INDEX feature_tlid_idx ON feature (tlid);
+CREATE INDEX feature_name_phone_zip_idx ON feature (name_phone, zip);
+CREATE INDEX range_tlid_idx ON range (tlid);
