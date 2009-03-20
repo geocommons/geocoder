bc92c17437bb5f209594387f0eeb213cf8ed879d
Schuyler Erle <schuyler@nocat.net>
2009-03-20 14:29:37 -0400

    Further work on address and constants.

--- lib/geocoder/us/address.rb
+++ lib/geocoder/us/address.rb
@@ -1,5 +1,30 @@
-module Geocoder
-end
+require 'geocoder/us/constants'
 
 module Geocoder::US
+  Fields = [
+    :prenum,
+    :number,
+    :sufnum,
+    :fraction,
+    :predir,
+    :prequal,
+    :pretyp,
+    :street,
+    :suftyp,
+    :sufqual,
+    :sufdir,
+    :unittyp,
+    :unit,
+    :city,
+    :state,
+    :zip,
+    :plus4
+  ]
+  class Address
+    attr_accessor :text
+    
+    def initialize (text)
+      @text = text
+    end
+  end
 end
