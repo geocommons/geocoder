172709f7ff601bf65696d3ac8e5297ae70861714
Schuyler Erle <sderle@goldman.(none)>
2009-04-02 17:27:18 -0400

    Additional changes to support new place table format.

--- sql/index.sql
+++ sql/index.sql
@@ -5,7 +5,7 @@ PRAGMA synchronous=OFF;
 PRAGMA cache_size=500000;
 PRAGMA count_changes=0;
 CREATE INDEX place_city_phone_state_idx ON place (city_phone, state);
-CREATE INDEX place_zip_paflag_idx ON place (zip, priority);
+CREATE INDEX place_zip_priority_idx ON place (zip, priority);
 CREATE INDEX edge_tlid_idx ON edge (tlid);
 CREATE INDEX feature_tlid_paflag_idx ON feature (tlid, paflag);
 CREATE INDEX feature_street_phone_zip_idx ON feature (street_phone, zip);
