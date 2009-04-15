7b4281fcbb99514a1dbca23d0a717bf30b3ebf91
Andrew Turner <andrew@highearthorbit.com>
2009-04-14 20:11:45 -0400

    Adding batch uploading to demo

--- demo/ws.rb
+++ demo/ws.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'geocoder/us/database'
+require 'fastercsv'
 
 set :port, 8080
 
@@ -9,12 +10,39 @@ get '/' do
     db = Geocoder::US::Database.new("/mnt/tiger2008/geocoder.db")
     @records = db.geocode params[:address]
   end
+
   case params[:format]
   when /atom/
-	builder :index
-else
-  erb :index
+    builder :index
+  else
+    erb :index
+  end
 end
+
+get '/batch' do 
+  FileUtils.mkdir_p('uploads/')
+  FileUtils.mv(params[:uploaded_csv][:tempfile].path, "uploads/#{params[:uploaded_csv][:filename]}")  
+  csv = "uploads/#{params[:uploaded_csv][:filename]}"
+  db = Geocoder::US::Database.new("/mnt/tiger2008/geocoder.db")
+  csv = FasterCSV.parse(open(csv_file))
+  headers = csv[0]
+  
+  @records = csv.collect do |record|
+    next if record == headers
+    begin
+      (db.geocode record[1]).first
+    rescue
+      next
+    end
+  end
+  
+  case params[:format]
+  when /atom/
+    builder :index
+  else
+    erb :index
+  end
+  
 end
 
 
