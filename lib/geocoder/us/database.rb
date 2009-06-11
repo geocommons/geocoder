3466fafc37fea302e84a39b0e490594689edac6d
Schuyler Erle <sderle@vishnu.tridity.org>
2009-06-11 16:45:43 +0000

    Changed my mind. Place canonicalization before scoring seems to make sense.

--- lib/geocoder/us/database.rb
+++ lib/geocoder/us/database.rb
@@ -481,6 +481,10 @@ module Geocoder::US
       merge_rows! candidates, places, :zip
 
       assign_number! query, candidates
+
+      # lookup.rst (11)
+      # see note above about canonicalizing places before scoring
+      canonicalize_places! candidates if canonicalize
   
       # lookup.rst (7)
       score_candidates! query, candidates
@@ -513,9 +517,6 @@ module Geocoder::US
         record[:lon], record[:lat] = interpolate points, dist
       }
       
-      # lookup.rst (11)
-      canonicalize_places! candidates if canonicalize
-   
       # lookup.rst (12)
       candidates.each {|record| clean_record! record}
       candidates
