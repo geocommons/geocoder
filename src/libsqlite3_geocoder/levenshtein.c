7bfbe02ac03e61b9a79d245cc2d5680b247d1390
Schuyler Erle <sderle@goldman.(none)>
2009-06-17 15:40:12 -0400

    Minor fix to correct broken behavior (inherited from Wikipedia, go fig).

--- src/libsqlite3_geocoder/levenshtein.c
+++ src/libsqlite3_geocoder/levenshtein.c
@@ -2,6 +2,8 @@
 # define STRLEN_MAX 256
 # define min(x, y) ((x) < (y) ? (x) : (y))
 # define max(x, y) ((x) > (y) ? (x) : (y))
+# define NO_CASE (~(unsigned char)32)
+# define eql(x, y) (((x) & NO_CASE) == ((y) & NO_CASE))
 
 static int d[STRLEN_MAX][STRLEN_MAX]; // this isn't thread safe
 
@@ -14,14 +16,15 @@ double levenshtein_distance (const unsigned char *s1, const unsigned char *s2) {
     for (i = 1; i <= len2; ++i) d[0][i] = i;
     for (i = 1; i <= len1; ++i) {
         for (j = 1; j <= len2; ++j) {
-            cost = (s1[i-1] == s2[j-1] ? 0 : 1);
+            cost = (eql(s1[i-1], s2[j-1]) ? 0 : 1);
             d[i][j] = min(min(
-                        d[i-1][j] + 1,              /* addition */
-                        d[i][j-1] + 1),             /* deletion */
-                        d[i-1][j-1] + cost);        /* substitution */
-            if (i > 1 && j > 1 && s1[i] == s2[j-1] && s1[i-1] == s2[j])
+                        d[i-1][j  ] + 1,              /* deletion */
+                        d[i  ][j-1] + 1),             /* insertion */
+                        d[i-1][j-1] + cost);          /* substitution */
+            if (i > 1 && j > 1 && eql(s1[i-1], s2[j-2]) && eql(s1[i-2], s2[j-1])) {
                d[i][j] = min( d[i][j],
-                              d[i-2][j-2] + cost);   /* transposition */
+                              d[i-2][j-2] + cost );   /* transposition */
+            }
         }
     }
     return (d[len1][len2] / (double) max(len1, len2));
