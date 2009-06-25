c4f85f5d2cf115fb226c9b6cb26ee4d0d684c389
Schuyler Erle <sderle@geocoder.us>
2009-06-25 23:13:01 +0000

    Need this index for canonicalizing street names, if that's important.

--- sql/index.sql
+++ sql/index.sql
@@ -8,5 +8,6 @@ PRAGMA count_changes=0;
 CREATE INDEX place_city_phone_state_idx ON place (city_phone, state);
 CREATE INDEX place_zip_priority_idx ON place (zip, priority);
 CREATE INDEX feature_street_phone_zip_idx ON feature (street_phone, zip);
+CREATE INDEX feature_tlid_paflag_idx ON feature (tlid, paflag);
 CREATE INDEX edge_tlid_idx ON edge (tlid);
 CREATE INDEX range_tlid_idx ON range (tlid);
