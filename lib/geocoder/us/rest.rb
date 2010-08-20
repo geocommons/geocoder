8859b41981b89f4de110297894240a3670a88f11
Schuyler Erle <schuyler@simplegeo.com>
2010-08-20 14:37:19 -0700

    Revise REST server to output GeoJSON.

--- lib/geocoder/us/rest.rb
+++ lib/geocoder/us/rest.rb
@@ -3,11 +3,25 @@ require 'geocoder/us/database'
 require 'json'
 
 @@db = Geocoder::US::Database.new(ARGV[0] || ENV["GEOCODER_DB"])
+
 get '/geocode' do
   if params[:q]
+    results = @@db.geocode params[:q]
+    features = []
+    results.each do |result|
+      coords = [result.delete(:lon), result.delete(:lat)]
+      features << {
+        :type => "Feature",
+        :properties => result,
+        :geometry => {
+          :type => "Point",
+          :coordinates => [result[:lon], result[:lat]]
+        }
+      }
+    end
     {
-      :results => @@db.geocode(params[:q]),
-      :address => params[:q]
+      :type => "FeatureCollection"
+      :features => features
     }.to_json
   else
     status 400
